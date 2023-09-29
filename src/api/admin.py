8c39ffdcaad0d7e5a30477f145875795383fe354
connorpobrien <54494032+connorpobrien@users.noreply.github.com>
2023-09-29 08:26:12 -0700

    reset

--- src/api/admin.py
+++ src/api/admin.py
@@ -16,13 +16,27 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
+    # Set gold to 100
+    with db.engine.begin() as connection:
+        sql_query = """UPDATE global_inventory SET gold = 100"""
+        connection.execute(sqlalchemy.text(sql_query))
+
+    # Set num_red_ml to 0
+    with db.engine.begin() as connection:
+        sql_query = """UPDATE global_inventory SET num_red_ml = 0"""
+        connection.execute(sqlalchemy.text(sql_query))
+
+    # Set num_red_potions to 0
+    with db.engine.begin() as connection:
+        sql_query = """UPDATE global_inventory SET num_red_potions = 0"""
+        connection.execute(sqlalchemy.text(sql_query))
+
     return "OK"
 
 
 @router.get("/shop_info/")
 def get_shop_info():
     """ """
-
     # TODO: Change me!
     return {
         "shop_name": "connors-potions",
