9841ac95118bbcd75d1ea1f69131753712e58cca
connorpobrien <54494032+connorpobrien@users.noreply.github.com>
2023-10-09 22:19:32 -0700

    updated cart logic

--- src/api/carts.py
+++ src/api/carts.py
@@ -25,19 +25,21 @@ def create_cart(new_cart: NewCart):
     count += 1
 
     # add id -> customer to carts
-    carts[count] = {}
-    return {"cart_id": count}
+    count += 1
+    carts[count] = {"customer": new_cart.customer, "items": {}}
+    return {"cart_id": str(count)}
 
 
 @router.get("/{cart_id}")
 def get_cart(cart_id: int):
     """ """
     # retrieve customer's cart based on the cart_id
-    # global count
-    # global carts
+    global carts
+    if cart_id not in carts:
+        print("Cart does not exist")
+        return 0
+    return carts[cart_id]
 
-    # return carts[cart_id]
-    return {}
     
 class CartItem(BaseModel):
     quantity: int
@@ -49,12 +51,10 @@ def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
     global count
     global carts
 
-    # if cart_id not in carts:
-    #     return "Cart does not exist"
-    
-    # else update quantity
-    # else: 
-    #     carts[cart_id] = cart_item.quantity
+    if cart_id not in carts:
+        print("Cart does not exist")
+        return 0
+    carts[cart_id]["items"][item_sku] = cart_item.quantity
 
     return "OK"
 
@@ -68,19 +68,22 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
     global count
     global carts
 
+    if cart_id not in carts:
+        print("Cart does not exist")
+        return 0
+
     cart = carts[cart_id]
 
 
-    # For now customers will only be able to buy 1 potion for each color
-    # Update gold
-    with db.engine.begin() as connection:
-        sql_query = """UPDATE global_inventory SET gold = gold + 50"""
-        connection.execute(sqlalchemy.text(sql_query))
+    # count number of potions bought
+    num_potions = sum(cart["items"].values())
 
-    # Update number of potions
-    with db.engine.begin() as connection:
-        sql_query = """UPDATE global_inventory SET num_red_potions = num_red_potions + 1"""
-        connection.execute(sqlalchemy.text(sql_query))
+    # count amount of gold paid
+    amount_gold = num_potions * 1 # cost = 1 for now
 
-    return {"total_potions_bought": 1, "total_gold_paid": 50}
+
+    # reset cart
+    carts[cart_id]["items"] = {}
+
+    return {"total_potions_bought": num_potions, "total_gold_paid": amount_gold}
 
