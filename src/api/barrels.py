91047b1f5847275ec1f51082c13027fd4f6ff27d
connorpobrien <54494032+connorpobrien@users.noreply.github.com>
2023-10-13 22:07:38 -0700

    barrels

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -61,56 +61,28 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
 @router.post("/plan")
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     """ """
-    # print(wholesale_catalog)
+    print(wholesale_catalog)
 
-    # I need more gold !!
+    # get gold value from global_inventory
     with db.engine.begin() as connection:
-        # add gold to inventory
-        sql_query = """UPDATE global_inventory SET gold = gold + 10000"""
-        connection.execute(sqlalchemy.text(sql_query))
+        sql_query = """SELECT gold from global_inventory"""
+        gold = connection.execute(sqlalchemy.text(sql_query)).first().gold
 
-
-    with db.engine.begin() as connection:
-        # Get the number of potions in inventory for each color
-        sql_query = """SELECT num_red_potions, num_green_potions, num_blue_potions from global_inventory"""
-        result = connection.execute(sqlalchemy.text(sql_query))  
-        first_row = result.first()
-        num_red_potions = first_row.num_red_potions
-        num_green_potions = first_row.num_green_potions
-        num_blue_potions = first_row.num_blue_potions
-
-    # Purchase a new small potion barrel only if the number of potions in inventory is less than 10
     res = []
-    if num_red_potions < 10:
-        res.append({
-            "sku": "SMALL_RED_BARREL",
-            "quantity": 1,
-        })
-    if num_green_potions < 10:
-        res.append({
-            "sku": "SMALL_GREEN_BARREL",
-            "quantity": 1,
-        })
-    if num_blue_potions < 10:
-        res.append({
-            "sku": "SMALL_BLUE_BARREL",
-            "quantity": 1,
-        })
-
-    # Temporarily hard coding
-    # return res
-    return [
-        {
-            "sku": "SMALL_RED_BARREL",
-            "quantity": 1000,
-        },
-        {
-            "sku": "SMALL_GREEN_BARREL",
-            "quantity": 1000,
-        },
-        {
-            "sku": "SMALL_BLUE_BARREL",
-            "quantity": 1000,
-        },
-    ]
-
+    # iterate through barrels and buy if possible
+    for barrel in wholesale_catalog:
+        if barrel.price <= gold:
+            res.append({
+                "sku": barrel.sku,
+                "quantity": 1, # only buying one for now
+            })
+            # update gold
+            gold -= barrel.price
+            
+    # Update global_inventory with new gold value
+    with db.engine.begin() as connection:
+        query = sqlalchemy.text("""UPDATE global_inventory SET gold = :gold""")
+        connection.execute(query, {"gold": gold})
+    
+    return res
+        
