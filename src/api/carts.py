09db121f1af5091523a5ce0207ae33dabe8884f3
connorpobrien <54494032+connorpobrien@users.noreply.github.com>
2023-10-15 00:31:06 -0700

    more changes

--- src/api/carts.py
+++ src/api/carts.py
@@ -71,8 +71,31 @@ class CartCheckout(BaseModel):
 @router.post("/{cart_id}/checkout")
 def checkout(cart_id: int, cart_checkout: CartCheckout):
     """ Handles the checkout process for a specific cart. """
-    num_potions, amount_gold = 0, 0
+    # Get the items in the cart
+    with db.engine.begin() as connection:
+        sql_query = """SELECT item_sku, quantity FROM cart_items WHERE cart_id = :cart_id"""
+        result = connection.execute(sqlalchemy.text(sql_query), {"cart_id": cart_id})
+        cart_items = result.fetchall()
 
+        # update catalog
+        for potion in cart_items:
+            pass
 
-    return {"total_potions_bought": num_potions, "total_gold_paid": amount_gold}
+        # calculate purchase amount - join with catalog
+
+        purchase_cost = 0
+
+        # update gold in global_inventory
+        sql_query = """UPDATE global_inventory SET gold = gold + :purchase_cost"""  
+        connection.execute(sqlalchemy.text(sql_query), {"purchase_cost": purchase_cost})
+
+        # delete cart
+        sql_query = """DELETE FROM carts WHERE cart_id = :cart_id"""
+        connection.execute(sqlalchemy.text(sql_query), {"cart_id": cart_id})
+
+        # delete cart_items
+        sql_query = """DELETE FROM cart_items WHERE cart_id = :cart_id"""
+        connection.execute(sqlalchemy.text(sql_query), {"cart_id": cart_id})
+
+    return {"total_potions_bought": 0, "total_gold_paid": purchase_cost}
 
--- src/api/catalog.py
+++ src/api/catalog.py
@@ -7,51 +7,6 @@ router = APIRouter()
 @router.get("/catalog/", tags=["catalog"])
 def get_catalog():
     """
-    Each unique item combination must have only a single price.
+    Retrieves the catalog of items. Each unique item combination must have only a single price.
     """
-    # Get the number of potions available
-    with db.engine.begin() as connection:
-        sql_query = """SELECT num_red_potions, num_green_potions, num_blue_potions from global_inventory"""
-        result = connection.execute(sqlalchemy.text(sql_query))  
-        first_row = result.first()
-        num_red_potions = first_row.num_red_potions
-        num_green_potions = first_row.num_green_potions
-        num_blue_potions = first_row.num_blue_potions
-
-    # Adding logic for buying red, green, and blue potions
-    # For now only can buy one
-
-    res = []
     
-    # hard coding to ensure we sell a red, green potion. Will fix. Also setting price to 1 temporarily
-    # if num_red_potions > 0:
-    if True:
-        res.append({
-            "sku": "RED_POTION_0",
-            "name": "red potion",
-            "quantity": 1,
-            "price": 1,
-            "potion_type": [100, 0, 0, 0],
-        })
-
-    # if num_green_potions > 0:
-    if True:
-        res.append({
-            "sku": "GREEN_POTION_0",
-            "name": "green potion",
-            "quantity": 1,
-            "price": 1,
-            "potion_type": [0, 100, 0, 0],
-        })
-
-    # if num_blue_potions > 0:
-    if True: 
-        res.append({
-            "sku": "BLUE_POTION_0",
-            "name": "blue potion",
-            "quantity": 1,
-            "price": 1,
-            "potion_type": [0, 0, 100, 0],
-        })
-    
-    return res
