00b88bd823973e2a9f96a8618b689e71c6c76afc
connorpobrien <54494032+connorpobrien@users.noreply.github.com>
2023-10-15 20:25:47 -0700

    admin changes

--- src/api/admin.py
+++ src/api/admin.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel
 from src.api import auth
 import sqlalchemy
 from src import database as db
+import random
+import string
 
 router = APIRouter(
     prefix="/admin",
@@ -18,16 +20,41 @@ def reset():
     """
     with db.engine.begin() as connection:
         # Reset gold to 100. Set all ml to 0
-        reset_global_inventory = """UPDATE global_inventory SET gold = 10000,
+        reset_global_inventory = """UPDATE global_inventory SET gold = 100,
                                                 num_red_ml = 0,
                                                 num_blue_ml = 0,
                                                 num_green_ml = 0,
                                                 num_dark_ml = 0"""
         connection.execute(sqlalchemy.text(reset_global_inventory))
 
-        # Clear catalog - set number of potions to 0
+        # Clear catalog
         reset_catalog = """DELETE FROM catalog"""
         connection.execute(sqlalchemy.text(reset_catalog))
+
+        # repopulate catalog
+        generate_sku = (lambda: ''.join(random.choice(string.ascii_letters + string.digits + "_") for _ in range(random.randint(1, 20))))
+        random_skus = set()
+        for _ in range(20):
+            random_skus.add(generate_sku())
+        unique_skus = list(random_skus) 
+        possible_potions = [[100, 0, 0, 0],
+                            [0, 100, 0, 0],
+                            [0, 0, 100, 0],
+                            [0, 0, 0, 100],
+                            [50, 50, 0, 0],
+                            [50, 0, 50, 0],
+                            [50, 0, 0, 50],
+                            [0, 50, 50, 0],
+                            [0, 50, 0, 50],
+                            [0, 0, 50, 50]] # I think more potion types will be added in future?
+        build_catalog = """INSERT INTO catalog (sku, quantity, price, red_ml, green_ml, blue_ml, dark_ml)
+                            VALUES (:sku, :quantity, :price, :red_ml, :green_ml, :blue_ml, :dark_ml)"""
+        for i in range(10):
+            sku = unique_skus.pop()
+            quantity = 0
+            price = 1
+            red_ml, green_ml, blue_ml, dark_ml = possible_potions[i]
+            connection.execute(sqlalchemy.text(build_catalog), {"sku": sku, "quantity": quantity, "price": price, "red_ml": red_ml, "green_ml": green_ml, "blue_ml": blue_ml, "dark_ml": dark_ml})
 
         # Delete all carts and cart items
         reset_carts = """DELETE FROM carts"""
