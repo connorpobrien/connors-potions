747dd8178de25f6210d03b1d8877497c6ec58bb7
connorpobrien <54494032+connorpobrien@users.noreply.github.com>
2023-10-15 19:59:57 -0700

    weird

--- src/api/audit.py
+++ src/api/audit.py
@@ -19,20 +19,18 @@ def get_inventory():
         # get data from global_inventory
         sql_query = """SELECT gold, num_red_ml, num_green_ml, num_blue_ml, num_dark_ml FROM global_inventory"""
         global_inventory = connection.execute(sqlalchemy.text(sql_query)).first()
-        gold = global_inventory.gold
+        total_gold = global_inventory.gold
         red_ml = global_inventory.num_red_ml
         green_ml = global_inventory.num_green_ml
         blue_ml = global_inventory.num_blue_ml
         dark_ml = global_inventory.num_dark_ml
-        ml_in_barrels = sum([red_ml, green_ml, blue_ml, dark_ml])
+        total_ml = sum([red_ml, green_ml, blue_ml, dark_ml])
         
         # Get data from catalog
         sql_query = """SELECT SUM(quantity) FROM catalog"""
         total_potions = connection.execute(sqlalchemy.text(sql_query)).first()
     
-    return {"number_of_potions": total_potions, 
-            "ml_in_barrels": ml_in_barrels,
-            "gold": gold}
+    return {"number_of_potions": total_potions, "ml_in_barrels": total_ml, "gold": total_gold}
 
 class Result(BaseModel):
     gold_match: bool
