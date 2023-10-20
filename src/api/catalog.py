ea6c50cf1317b926ccf3bc66387e4bd47c757ca8
connorpobrien <connor.p.obrien@gmail.com>
2023-10-19 19:25:38 -0700

    catalog done

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -9,23 +9,38 @@ def get_catalog():
     """
     Retrieves the catalog of items. Each unique item combination must have only a single price.
     """
-    # query catalog
     with db.engine.begin() as connection:
-        sql_query = """SELECT sku, name, quantity, price, num_red_ml, num_green_ml, num_blue_ml, num_dark_ml FROM catalog"""
-        catalog = connection.execute(sqlalchemy.text(sql_query))
-        catalog = catalog.fetchall()
+        # form catalog
+        combined_query = """SELECT 
+                                    catalog.sku, 
+                                    catalog.name, 
+                                    catalog.price, 
+                                    catalog.red_ml, 
+                                    catalog.green_ml, 
+                                    catalog.blue_ml, 
+                                    catalog.dark_ml,
+                                    COALESCE(ledger.total, 0) AS quantity
+                                FROM 
+                                    catalog
+                                LEFT JOIN 
+                                    (SELECT 
+                                        sku, 
+                                        SUM(change) AS total 
+                                    FROM 
+                                        catalog_ledger 
+                                    GROUP BY 
+                                        sku) AS ledger
+                                ON 
+                                    catalog.sku = ledger.sku
+                            """
+        catalog = connection.execute(sqlalchemy.text(combined_query)).fetchall()
 
-    # For each row, if quantity > 0, add to return
-    res = []
-    for item in catalog:
-        # NEW - quantity will no longer be stored in catalog table. Need to SUM() from catalog ledger using potion sku
-        # find quantity based on sku from catalog_ledger table
-        sql_query = """SELECT SUM(change) AS total FROM catalog_ledger WHERE sku = :sku"""
-        result = connection.execute(sqlalchemy.text(sql_query), {"sku": item.sku})
-        quantity = result.first().total
+        # build output list
+        res = []
+        for item in catalog:
+            sku, name, price, red_ml, green_ml, blue_ml, dark_ml, quantity = item
+            if item.quantity > 0:
+                res.append({"sku": item.sku, "name": item.name, "quantity": item.quantity, "price": item.price, "potion_type": [item.num_red_ml, item.num_green_ml, item.num_blue_ml, item.num_dark_ml]})
+                print(f'''Item in catalog: \n sku: {item.sku} \n name: {item.name} \n quantity: {item.quantity} \n price: {item.price} \n potion_type: {item.num_red_ml, item.num_green_ml, item.num_blue_ml, item.num_dark_ml}''')
 
-        if item.quantity > 0:
-            res.append({"sku": item.sku, "name": item.name, "quantity": item.quantity, "price": item.price, "potion_type": [item.num_red_ml, item.num_green_ml, item.num_blue_ml, item.num_dark_ml]})
-            print(f'''Item in catalog: \n sku: {item.sku} \n name: {item.name} \n quantity: {item.quantity} \n price: {item.price} \n potion_type: {item.num_red_ml, item.num_green_ml, item.num_blue_ml, item.num_dark_ml}''')
-
-    return res
+        return res
