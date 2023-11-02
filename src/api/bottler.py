1c89a7619c01190d692cde53df43626e2bc7de3d
connorpobrien <connor.p.obrien@gmail.com>
2023-11-02 10:23:15 -0700

    bottle plan

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -98,7 +98,7 @@ def get_bottle_plan():
         total_potions = connection.execute(sqlalchemy.text(catalog_ledger_query)).first()[0] or 0
 
     # sort to find potions to replenish
-    catalog = sorted(catalog, key=lambda x: x.quantity, reverse=True)
+    catalog = sorted(catalog, key=lambda x: x.quantity)
 
     print(catalog)
 
@@ -112,10 +112,8 @@ def get_bottle_plan():
         for item in catalog:
             sku, name, price, red_ml, green_ml, blue_ml, dark_ml, quantity = item
             # if possible
-            if (inventory_red_ml >= red_ml) and (inventory_green_ml >= green_ml) and (inventory_blue_ml >= blue_ml) and (inventory_dark_ml >= dark_ml):
+            if (inventory_red_ml >= red_ml) and (inventory_green_ml >= green_ml) and (inventory_blue_ml >= blue_ml) and (inventory_dark_ml >= dark_ml) and len(bottle_plan) < 6:
                 potion_type = (red_ml, green_ml, blue_ml, dark_ml)
-                if potion_type not in bottle_plan and len(bottle_plan) == 6:
-                    continue
                 if potion_type in bottle_plan:
                     bottle_plan[potion_type]["quantity"] += 1
                 else:
